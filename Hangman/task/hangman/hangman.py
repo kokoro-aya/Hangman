7b4ca3e78171360b694ccabaa54c608da1e273fa
alicenychto <irol2273@icloud.com>
2020-07-18 14:10:18 +0200

    stage 3 cleared

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,8 +1,10 @@
 # Write your code here
+import random
 print("""
 H A N G M A N
 """)
-word = "python"
+word = ["python", "java", "kotlin", "javascript"]
 guess = input("Guess the word: ")
+secret = random.choice(word)
 
-print("You survived!" if word == guess else "You are hanged!")
+print("You survived!" if guess == secret else "You are hanged!")
