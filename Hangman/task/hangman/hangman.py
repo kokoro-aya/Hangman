86a7029b5d31878016489350022f646a046f5a3b
alicenychto <irol2273@icloud.com>
2020-07-18 14:14:44 +0200

    stage 4 cleared

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,10 +1,14 @@
 # Write your code here
 import random
+
+def masked_word(word: str) -> str:
+    return word[0:3] + '-' * (len(word) - 3)
+
 print("""
 H A N G M A N
 """)
 word = ["python", "java", "kotlin", "javascript"]
-guess = input("Guess the word: ")
 secret = random.choice(word)
+guess = input("Guess the word " + masked_word(secret) + ": ")
 
 print("You survived!" if guess == secret else "You are hanged!")
