affdad821d17956f0a985f4f425a93a8a3f7fc02
alicenychto <irol2273@icloud.com>
2020-07-18 21:59:56 +0200

    stage 5

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,14 +1,31 @@
 # Write your code here
 import random
 
-def masked_word(word: str) -> str:
-    return word[0:3] + '-' * (len(word) - 3)
-
 print("""
 H A N G M A N
 """)
 word = ["python", "java", "kotlin", "javascript"]
 secret = random.choice(word)
-guess = input("Guess the word " + masked_word(secret) + ": ")
 
-print("You survived!" if guess == secret else "You are hanged!")
+guess = '-' * len(secret)
+guessed = []
+
+for _ in range(0, 8):
+    print(guess)
+    letter = input("Input a letter: ")
+    if letter in secret and letter not in guessed:
+        occurrence = [i for i in range(len(secret)) if secret[i] == letter]
+        for index in occurrence:
+            guess = guess[:index] + letter + guess[index + 1:]
+        guessed.append(letter)
+    else:
+        print("No such letter in the word")
+    print()
+
+
+print("""
+Thanks for playing!
+We'll see how well you did in the next stage
+""")
+
+# print("You survived!" if guess == secret else "You are hanged!")
